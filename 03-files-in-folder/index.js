2521429c0053886da2f1cb7aa22229632e6cf78a
Eugene Mikhalionok <eugenemikhalionok@gmail.com>
2022-05-22 00:58:02 +0400

    fead: HTML Bilder 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+(async () => {
+  const pathToFolder = path.join(__dirname, "secret-folder");
+  try {
+    const files = await fs.readdir(pathToFolder);
+
+    for (const file of files) {
+      const stats = await fs.stat(path.join(pathToFolder, file));
+      if (!stats.isDirectory()) {
+        const name = file.split(".")[0]; // remove leadingотбрасываем расширение
+        const ext = path.extname(file).slice(1); //отбрасываем точку
+        const size = Math.floor(stats.size / 1024); // преобразовываем в кб
+        console.log(`${name}-${ext}-${size}kb`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+})();
