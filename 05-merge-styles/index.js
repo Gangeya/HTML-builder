ee2c07558ee2d409c180e4111f846397ef88d3d2
Eugene Mikhalionok <eugenemikhalionok@gmail.com>
2022-05-22 17:20:04 +0400

    fead HTML BILDER #5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+const pathSrc = path.join(__dirname, "styles");
+// const pathDest = path.join(__dirname, "files-copy");
+
+const { stdout } = process;
+
+(async () => {
+  //Чтение содержимого папки styles
+  const files = await fs.promises.readdir(path.join(pathSrc));
+  // создаем поток для записи
+  const output = fs.createWriteStream(
+    path.join(__dirname, "project-dist", "bundle.css")
+  );
+
+  for (const file of files) {
+    //проверяем на файл и расширение css
+    const stats = await fs.promises.stat(path.join(pathSrc, file));
+    if (stats.isFile() && path.extname(file) === ".css") {
+      const promiseFileData = await fs.promises.readFile(
+        path.join(pathSrc, file),
+        "utf-8"
+      );
+      const fileData = await promiseFileData.toString();
+      output.write(fileData);
+    }
+    //console.log(file);
+  }
+})();
